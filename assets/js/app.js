f8c552181d7a5ea7a51c336840b8228fdcadd098
sanjuverma373 <sv0240866@gmail.com>
2023-11-25 15:57:24 +0530

    nav changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,34 @@
+let redact = document.querySelector(".redact")
+let extract = document.querySelector(".extract")
+let detect = document.querySelector(".detect")
+let compare = document.querySelector(".compare")
+
 function openNav() {
     document.getElementById("navBar").classList.toggle("start-0");
-    document.body.classList.toggle("overflow_hidden");
+    document.body.classList.toggle("overflow-hidden");
     document.querySelector(".menu").classList.toggle("cross")
 }
+
+redact.addEventListener("click",function(){
+    document.getElementById("navbar").classList.remove("start-0");
+    document.body.classList.remove("overflow_hidden");
+    document.querySelector(".menu").classList.remove("cross")
+})
+extract.addEventListener("click",function(){
+    document.getElementById("navbar").classList.remove("start-0");
+    document.body.classList.remove("overflow_hidden");
+    document.querySelector(".menu").classList.remove("cross")
+})
+detect.addEventListener("click",function(){
+    document.getElementById("navbar").classList.remove("start-0");
+    document.body.classList.remove("overflow_hidden");
+    document.querySelector(".menu").classList.remove("cross")
+})
+compare.addEventListener("click",function(){
+    document.getElementById("navbar").classList.remove("start-0");
+    document.body.classList.remove("overflow_hidden");
+    document.querySelector(".menu").classList.remove("cross")
+})
 
 let backToTop = document.getElementById('backtotop')
 backToTop.addEventListener("click", function () {
@@ -16,16 +42,16 @@ window.addEventListener('scroll', function () {
     }
 });
 
- const preloader = document.getElementById("preloader");
-        setTimeout(() => {
-            document.getElementById("preloader").classList.add("d_none")
-            document.body.classList.remove("flow-hidden")
+const preloader = document.getElementById("preloader");
+setTimeout(() => {
+    document.getElementById("preloader").classList.add("d_none")
+    document.body.classList.remove("flow-hidden")
 
-        }, 2000);
+}, 2000);
 
-  
 
-       
+
+
 
 
 
